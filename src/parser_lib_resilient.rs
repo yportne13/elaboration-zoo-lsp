2aace0871cd021a6826ffe23d3903d56f32f06c8
yportne <yportne@163.com>
2026-07-27 16:13:29 +0800

    refactor: tuple_cut_parser short-circuits on first parse error
    When a subsequent parser (after the mandatory first) fails, immediately
    return None for all remaining parsers instead of continuing to try them.

--- src/parser_lib_resilient.rs
+++ src/parser_lib_resilient.rs
@@ -251,7 +251,7 @@ macro_rules! tuple_cut_parser {
             Ok((i, v)) => (i, Some(v)),
             Err(e) => {
                 $state.push(e);
-                ($input, None)
+                return Ok(($input, ($($parsed,)* None $(, Option::<$t>::None)*)));
             }
         };
         tuple_cut_parser!($self => input => $state => $($parsed)+ val => $($t | $p | $idx),+)
